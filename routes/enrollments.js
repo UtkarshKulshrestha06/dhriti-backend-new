5fdfd81248932b935706a7940614f77783da345d
UtkarshKulshrestha06 <utkarshkulshr@gmail.com>
2026-01-27 23:14:31 +0530

    feat: add batch enrollment listing endpoint

--- routes/enrollments.js
+++ routes/enrollments.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const supabase = require("../supabaseClient");
-const { requireAuth, requireAdmin } = require("../middleware/auth");
+const { requireAuth, requireAdmin, requireTeacher } = require("../middleware/auth");
 
 /**
  * GET /enrollments/:userId
